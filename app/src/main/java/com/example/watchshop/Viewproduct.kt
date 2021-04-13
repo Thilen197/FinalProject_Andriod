b290fc16cd737ed316992507a7fc03dc25ca57d3
thilen197 <thilen197@gmail.com>
2021-04-13 17:15:03 +0545

    View product design

---
+++ app/src/main/java/com/example/watchshop/Viewproduct.kt
@@ -0,0 +1,11 @@
+package com.example.watchshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Viewproduct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewproduct)
+    }
+}
