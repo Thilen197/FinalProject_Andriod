cdebcaf6d87e10b1600d5847543a8693c3f1cf93
thilen197 <thilen197@gmail.com>
2021-04-15 17:05:24 +0545

    Product API

--- app/src/main/java/com/example/watchshop/repository/ProductRepository.kt
+++ app/src/main/java/com/example/watchshop/repository/ProductRepository.kt
@@ -1,4 +1,49 @@
 package com.example.watchshop.repository
 
-class ProductRepository {
+import com.example.watchshop.api.MyApiRequest
+import com.example.watchshop.api.ServiceBuilder
+
+class ProductRepository : MyApiRequest {
+
+        private val productAPI =
+            ServiceBuilder.buildService(ProductAPI::class.java)
+
+        //Add Student
+        suspend fun addProduct(product: Product): AddProductResponse {
+            return apiRequest {
+                productAPI.addProduct(
+                    ServiceBuilder.token!!, product
+                )
+            }
+        }
+        suspend fun addImage(product: Product): AddProductResponse {
+            return apiRequest {
+                productAPI.addProduct(
+                    ServiceBuilder.token!!, product
+                )
+            }
+        }
+        suspend fun getAllProduct(): AllProductResponse {
+            return apiRequest {
+                productAPI.getAllProduct(
+                    ServiceBuilder.token!!
+                )
+            }
+        }
+        suspend fun deleteProduct(id: String): DeleteProductResponse {
+            return apiRequest {
+                productAPI.deleteProduct(
+                    ServiceBuilder.token!!,id
+                )
+            }
+        }
+        suspend fun uploadImage(id: String, body: MultipartBody.Part)
+                : ImageResponse {
+            return apiRequest {
+                productAPI.uploadImage(ServiceBuilder.token!!, id, body)
+            }
+        }
+
+    }
+
 }
