2edeab1b30a364c299e33bf31f1df0f9f8570adc
thilen197 <thilen197@gmail.com>
2021-04-15 17:08:48 +0545

    Image Response Created

--- app/src/main/java/com/example/watchshop/api/ProductAPI.kt
+++ app/src/main/java/com/example/watchshop/api/ProductAPI.kt
@@ -1,4 +1,37 @@
 package com.example.watchshop.api
 
 interface ProductAPI {
+
+    @POST("order/insert")
+    suspend fun addProduct(
+        @Header("Authorization") token : String,
+        @Body product : Product
+    ) : Response<AddProductResponse>
+    @POST("/image")
+    suspend fun addImage(
+        @Header("Authorization") token : String,
+        @Body product : Product
+    ) : Response<AddProductResponse>
+    // get all students
+    @GET("order/fetch")
+    suspend fun getAllProduct(
+        @Header("Authorixation") token : String
+    ) : Response<AllProductResponse>
+
+    //delete student
+    @DELETE("order/delete/{id}")
+    suspend fun deleteProduct(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ):Response<DeleteProductResponse>
+
+    @Multipart
+    @PUT("student/{id}/photo")
+    suspend fun uploadImage(
+        @Header("Authorization") token: String,
+        @Path ("id") id:String,
+        @Part file: MultipartBody.Part
+    ):Response<ImageResponse>
+}
+
 }
