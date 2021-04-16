7004befdcd46399935ffd3dd611716ce0b41c926
thilen197 <thilen197@gmail.com>
2021-04-16 15:08:45 +0545

    Login Test

--- app/src/test/java/com/example/watchshop/WatchshopTest.kt
+++ app/src/test/java/com/example/watchshop/WatchshopTest.kt
@@ -1,4 +1,19 @@
 package com.example.watchshop
 
+import com.example.watchshop.repository.CustomerRepository
+import junit.framework.Assert
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
 class WatchshopTest {
+    private lateinit var customer: CustomerRepository;
+    @Test
+    fun loginTest() = runBlocking {
+        customer = CustomerRepository();
+        val response = customer.checkCustomer("thilen2021","thilen2021")
+        val expectedResult: Boolean = true;
+        val actualResult = response.success;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
 }
